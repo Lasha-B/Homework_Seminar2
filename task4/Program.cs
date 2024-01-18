f7e91877a9c7d4dd1a19f1b10a46055c240a6c7d
Lasha <bg.7788211@gmail.com>
2024-01-19 01:01:46 +0300

    add task4

---
+++ task4/Program.cs
@@ -0,0 +1,17 @@
+﻿int number = 6532;
+int result = 0;
+int y = number.ToString().Length;
+ 
+while (number > 0)
+{
+result *= 10;
+result += number % 10;
+number /= 10;
+}
+ 
+for (int i = 0; i < y; i++)
+{
+Console.Write(result % 10 + ",");
+result /= 10;
+}
+Console.ReadKey();
